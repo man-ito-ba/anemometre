8400d9f7bfb5326521fd6cecb0419f30a6de0e64
man-ito-ba <pierre.m.global@gmail.com>
2016-06-14 11:53:47 +0200

    Explication plus précises dans les commentaires

--- anemometre.ino
+++ anemometre.ino
@@ -1,5 +1,8 @@
-/*La solution ici consiste à calculer une moyenne mobile sur un nombre d'échantillons précis. Par exemple : les dix derniers échantillons.*/
-const int NbLectures = 10;    //Définit le nombre d'échantillons à conserver pour calculer la moyenne ; plus le chiffre est elevé, plus le tableau sera "lissé", mais plus le programme sera lent. Utiliser  une constante plutôt qu'une variable normale permet d'utiliser cette valeur pour déterminer la taille du tableau.
+/*
+  La solution ici consiste à calculer une moyenne mobile sur un nombre d'échantillons précis. 
+  Par exemple : les dix derniers échantillons pour une analyse sur 1 secondes
+  (et donc les 30 derniers pour une analyse sur 3 secondes*/
+const int NbLectures = 30;    //Définit le nombre d'échantillons à conserver pour calculer la moyenne ; plus le chiffre est elevé, plus le tableau sera "lissé", mais plus le programme sera lent. Utiliser  une constante plutôt qu'une variable normale permet d'utiliser cette valeur pour déterminer la taille du tableau.
 
 int Tableau[NbLectures];      // Tableau recevant les signaux analogiques
 int IndexTableau = 0;         // l'index de position du tableau
